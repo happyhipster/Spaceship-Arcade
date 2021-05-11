5afa9e78018137939c8b36e9276a624d7a8026f6
happyhipster <yoonbin.au@gmail.com>
2021-05-11 21:53:56 +1000

    user input removed and changed to animation

--- spooceship.py
+++ spooceship.py
@@ -11,14 +11,11 @@ def stars():
 
 
 #Lines during the 'warp phase'
-def speed_lines(wow):
-        if wow == 1:
-            for i in range(100):
-                x1 = random.randint(0,1501)
-                y1 = random.randint(0,751)
-                arcade.draw_line(x1, y1, x1+50 ,y1, arcade.color.WHITE_SMOKE, 2)
-        else:
-            arcade.draw_line(900, 900, 900 ,900, arcade.color.BLACK, 1)
+def speed_lines():
+        for i in range(100):
+            x1 = random.randint(0,1501)
+            y1 = random.randint(0,751)
+            arcade.draw_line(x1, y1, x1+50 ,y1, arcade.color.WHITE_SMOKE, 2)
 
 
 #Drawing the spooceship
@@ -34,19 +31,9 @@ def spoceship(x,y):
     arcade.draw_polygon_filled(fire_inside_point_list, arcade.color.YELLOW)
 
 
-#Drawing the explosion
-def explosion():
-    arcade.draw_circle_filled(on_draw.x, on_draw.y, 300, arcade.color.RED, 10)
-    arcade.draw_circle_filled(on_draw.x, on_draw.y, 150, arcade.color.YELLOW, 10)
-
-
 #Declaring the variables
 SPOOD = 0
 SPEED = 5
-b = 0
-a = 1
-space = 2
-boom = 0
 
 
 WINDOW_HEIGHT = 750
@@ -57,7 +44,7 @@ WINDOW_LENGTH = 1500
 def on_draw(delta_time):
     #access global variables in this function
     global SPEED
-    global boom
+    global SPOOD
     arcade.start_render()
     #Calling functions to draw scenery
     stars()
@@ -68,33 +55,22 @@ def on_draw(delta_time):
     on_draw.y += SPOOD
 
     #conditional: when the speed of the ship is more than 300, the speed_lines function is called.
-    #If the speed of the ship is lower than 300, then the speed_lines are gone.
-    if SPEED > 300:
-        speed_lines(1)
-    else:
-        speed_lines(0)
+    if SPEED >= 300:
+        speed_lines()
+        SPEED = 300
 
+    #conditional: if on_draw.x is more than the WINDOW_LENGTH+150, set on_draw.x to -250 then times SPEED by 1.5 (once it reaches the end of the window, the spooceship goes back to the start again)
+    if on_draw.x > WINDOW_LENGTH + 150:
+        on_draw.x = -250
+        SPEED *= 1.5
 
-    #conditionals: if a (counter of key 'o') is divisible by 10, cover screen with black circle and add text, "hehe spooceship go brr" then set speed to 700
-    if a%10 == 0:
-        arcade.draw_circle_filled(0,0,1000000000000,arcade.color.BLACK,1)
-        arcade.draw_text("hehe spooceship go brr", 600, 400, arcade.color.WHITE, 50, width=750, align="center", anchor_x="center", anchor_y="center")
-        SPEED = 700
-    
-    #conditionals: if boom (counter of key 'e') if on_draw.x is bigger than 50, set speed to 75 then call explosion function. 
-    if boom == 15:
-        if on_draw.x >50:
-            SPEED = 75
-            explosion()
-    else:
-        if on_draw.x > WINDOW_LENGTH + 150:       
-            on_draw.x = -250
+    #conditional: if on_draw.y is more than WINDOW_HEIGHT-700, change SPOOD by -2
+    if on_draw.y > WINDOW_HEIGHT-700:
+        SPOOD += -2
 
-        if on_draw.y > WINDOW_HEIGHT-100:
-            on_draw.y = WINDOW_HEIGHT-100
-
-        if on_draw.y < 100:
-            on_draw.y = 100
+    #conditional: else if on_draw.y is less than 700, change SPOOD by 2
+    elif on_draw.y < 700:
+        SPOOD += 2
 
 
 on_draw.x = -200
@@ -110,45 +86,7 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.BLACK)
 
     
-    #function for user input
-    def on_key_press(self, key, modifiers):
-        global b
-        global SPEED
-        global speed_lines
-        global a
-        global boom
-        global space
-
-        #this code speaks for itself (?)
-        if key == arcade.key.UP or key == arcade.key.W:
-            on_draw.y += 20
-
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            on_draw.y += -20
-
-        if key == arcade.key.RIGHT or key == arcade.key.D:
-            SPEED *= 1.5
-            if SPEED > 700:
-                SPEED = 700
-
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            SPEED /= 1.3
-
-        if key == arcade.key.O:
-            a += 1
-            print(a)
-        
-        if key == arcade.key.E:
-            boom += 1
-            print(boom)
-
-        if key == arcade.key.SPACE:
-            space += 1
-            if space%2 == 1:
-                SPEED = 0
-            elif space%2 == 0:
-                SPEED = 10
-
+    
 def main():
     #Set canvas then run
     MyGame(WINDOW_LENGTH, WINDOW_HEIGHT, "SPOOCESHIP")
